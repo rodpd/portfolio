c3de942e2657d0a24f0b8648a9e2b261ecca07a9
rodpd <rodrigo.pad@outlook.com.br>
2022-07-25 20:14:58 -0300

    update projects

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="mb-10 lg:grid lg:grid-cols-2">
         <div class="my-auto">
-            <img :src="require(`../assets/${img}`)" alt="" class="w-10/12 mx-auto duration-300 sm:w-3/4 hover:scale-110">
+			<a :href="`${demoUrl}`">
+				<img :src="require(`../assets/${img}`)" alt="" class="w-10/12 mx-auto duration-300 sm:w-3/4 hover:scale-110">
+			</a>
         </div>
         <div class="m-10 lg:ml-10 lg:mr-48">
             <h2 class="text-3xl text-left text-slate-200">{{title}}</h2>
@@ -10,8 +12,8 @@
                 <Skills :skills="this.skills" />
             </div>
             <div class="flex gap-6 mt-6">
-                <GithubIcon color="slate-200" size="40" link="https://www.github.com/rodpd/portal-de-filmes" />
-                <a href="https://diw.rodpd.repl.co" target="_blank">
+                <GithubIcon color="slate-200" size="40" :link="`${githubUrl}`" />
+                <a :href="`${demoUrl}`" target="_blank">
                     <WebIcon />
                 </a>
             </div>
@@ -33,7 +35,7 @@ export default {
     WebIcon,
     GithubIcon
 },
-    props: ['title', 'description', 'img', 'skills', 'linkGithub', 'linkProject']
+    props: ['title', 'description', 'img', 'skills', 'linkGithub', 'linkProject', 'githubUrl', 'demoUrl']
 }
 </script>
 
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,7 +4,14 @@
             <h1 class="text-5xl text-slate-200">Projetos</h1>
             <Bar color="slate-200" />
         </div>
-        <ProjectItem v-for="project in this.projects" :key="project.id" :title="project.title" :description="project.description" :img="project.img" :skills="project.skills" />
+        <ProjectItem v-for="project in this.projects" :key="project.id" 
+		:title="project.title" 
+		:description="project.description" 
+		:img="project.img" 
+		:skills="project.skills" 
+		:githubUrl="project.githubUrl"
+		:demoUrl="project.demoUrl"
+		/>
     </div>
 </template>
 
@@ -24,16 +31,40 @@ export default {
     data () {
         return {
             projects: [
-                { id: 1, title: "Portal de Filmes", img: "movies.png",
-                description: "Portal que exibe informações sobre filmes e séries, obtidas utilizando Fetch API para consumir a API do The Movie Database (https://developers.themoviedb.org/)",
-                skills: [
-                    {id: 1, text: 'HTML'},
-                    {id: 2, text: 'CSS'},
-                    {id: 3, text: 'JavaScript'},
-                    {id: 4, text: 'Bootstrap'},
-                    {id: 5, text: 'Fetch API'},
-                    {id: 6, text: 'REST API'}
-                ]}]
+				{ 
+					id: 1, title: "World Info - Em construção", img: "world.png",
+					description: ` Site desenvolvido com Vue.js e TailwindCSS que exibe diversas informações sobre os países em cards. O projeto usa a 
+					API Rest Countries (https://restcountries.com/) para obter os dados exibidos na interface. Ainda em construção,
+					serão adicionadas outras funcionalidades como busca, filtro por região e dark mode.
+					`,
+					skills: [
+						{id: 1, text: 'HTML'},
+						{id: 2, text: 'CSS'},
+						{id: 3, text: 'JavaScript'},
+						{id: 4, text: 'Vue.js'},
+						{id: 5, text: 'TailwindCSS'},
+						{id: 6, text: 'Fetch API'},
+                	],
+					githubUrl: "https://github.com/rodpd/world-info",
+					demoUrl: "https://diw.rodpd.repl.co"
+				},
+                { 
+					id: 2, title: "Portal de Filmes", img: "movies.png",
+					description: `Portal que exibe informações sobre filmes e séries, obtidas utilizando Fetch API para consumir 
+					a API do The Movie Database (https://developers.themoviedb.org/)
+					`,
+					skills: [
+						{id: 1, text: 'HTML'},
+						{id: 2, text: 'CSS'},
+						{id: 3, text: 'JavaScript'},
+						{id: 4, text: 'Bootstrap'},
+						{id: 5, text: 'Fetch API'},
+						{id: 6, text: 'REST API'}
+                	],
+					githubUrl: "https://github.com/rodpd/portal-de-filmes",
+					demoUrl: "https://diw.rodpd.repl.co"
+				}
+			]
         }
     }
 }
