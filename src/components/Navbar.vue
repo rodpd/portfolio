23b5511c1c8b7f6ff600ee538a7e2bf52df1b6ea
rodpd <rodrigo.pad@outlook.com.br>
2022-08-25 23:14:50 -0300

    fix navbar z-index, change calculator project image

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex justify-center w-screen gap-4 pr-10 lg:fixed sm:gap-8 bg-navyblue">
+    <nav class="z-10 flex justify-center w-screen gap-4 pr-10 lg:fixed sm:gap-8 bg-navyblue">
         <NavbarItem title="Home" reference="home" />
         <NavbarItem title="Projetos" reference="projects" />
         <NavbarItem title="Contato" reference="contact" />
